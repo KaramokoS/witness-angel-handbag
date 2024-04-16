6baf85053bc89ee1f686d484dfcca59d178cc664
karamoko <karamoko.samassa@inria.fr>
2024-04-16 17:13:52 +0200

    adding jasmine lib

---
+++ wacrypto-c/test/test_jsmn.c
@@ -0,0 +1,74 @@
+#include <jsmn.h>
+#include <unity.h>
+#include <string.h>
+
+static const char *JSON_STRING = 
+    "{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
+    "\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
+
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+  if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
+      strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
+void setUp(void)
+{
+  // set stuff up here
+}
+
+void tearDown(void)
+{
+  // clean stuff up here
+}
+
+void test_function_jsmn_simple_json(void)
+{
+    char* groups[] = {"users", "wheel", "audio", "video"};
+    int counter = 0;
+    int i;
+    int r;
+    jsmn_parser p;
+    jsmntok_t t[128]; /* We expect no more than 128 tokens */
+    jsmn_init(&p);
+    r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t,
+                sizeof(t) / sizeof(t[0]));
+    TEST_ASSERT_GREATER_THAN(0, r);
+
+    for (i = 1; i < r; i++) {
+        if (jsoneq(JSON_STRING, &t[i], "user") == 0) {
+            TEST_ASSERT_EQUAL_STRING("johndoe", strndup(JSON_STRING + t[i + 1].start, t[i + 1].end - t[i + 1].start));
+            i++;
+        } else if (jsoneq(JSON_STRING, &t[i], "admin") == 0) {
+            TEST_ASSERT_EQUAL_STRING("false", strndup(JSON_STRING + t[i + 1].start, t[i + 1].end - t[i + 1].start));
+            i++;
+        } else if (jsoneq(JSON_STRING, &t[i], "uid") == 0) {
+            TEST_ASSERT_EQUAL_STRING("1000", strndup(JSON_STRING + t[i + 1].start, t[i + 1].end - t[i + 1].start));
+            i++;
+        } else if (jsoneq(JSON_STRING, &t[i], "groups") == 0) {
+            int j;
+            if (t[i + 1].type != JSMN_ARRAY) {
+                continue; /* We expect groups to be an array of strings */
+            }
+            for (j = 0; j < t[i + 1].size; j++) {
+                jsmntok_t *g = &t[i + j + 2];
+                TEST_ASSERT_EQUAL_STRING(groups[counter], strndup(JSON_STRING + g->start, g->end - g->start));
+                counter += 1;
+            }
+            i += t[i + 1].size + 1;
+        } else {
+            TEST_ASSERT_GREATER_THAN(0, r);
+        }
+    }
+}
+
+void app_main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_function_jsmn_simple_json);
+
+    UNITY_END();
+}
